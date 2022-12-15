bfc053d42477e793231815034057b1f62a4db645
Atlee Circuitree <atleecircuitree@gmail.com>
2022-12-15 18:26:39 -0500

    Training Day 1 Progress

--- RobotCode/src/main/java/frc/robot/commands/ExampleCommand.java
+++ RobotCode/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -5,20 +5,21 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
+  private final ExampleSubsystem subsystem;
+  private XboxController xbox;
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public ExampleCommand(ExampleSubsystem paramSubsystem, XboxController paramXbox) {
+    xbox = paramXbox;
+    subsystem = paramSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
@@ -29,7 +30,11 @@ public class ExampleCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    subsystem.tankDrive(xbox.getLeftY(), xbox.getRightX());
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- RobotCode/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,11 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+  
+  private DifferentialDrive drive; 
+  private CANSparkMax frontLeftMotor;
+  private CANSparkMax frontRightMotor;
+  private CANSparkMax rearLeftMotor;
+  private CANSparkMax rearRightMotor;
+  private MotorControllerGroup leftMotors;
+  private MotorControllerGroup rightMotors;
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+
+    frontLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(1, MotorType.kBrushless);
+    rearLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
+    rearRightMotor = new CANSparkMax(4, MotorType.kBrushless);
+    
+    leftMotors = new MotorControllerGroup(frontLeftMotor, rearLeftMotor);
+    rightMotors = new MotorControllerGroup(frontRightMotor, rearRightMotor);
+    
+    drive = new DifferentialDrive(leftMotors, rightMotors);
+  }
+
+  public void tankDrive(Double speed, Double rotation){
+
+    drive.arcadeDrive(speed, rotation);
+
+  }
 
   @Override
   public void periodic() {
